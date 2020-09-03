fb176d342c1614aae3f960110f131f7a839e61bc
David Heyman <dave@axismaps.com>
2020-09-03 11:27:42 +0100

    Added learn more

---
+++ src/pages/learn-more.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import parse from 'html-react-parser';
+
+import Layout from '../components/layout';
+
+export const query = graphql`
+  {
+    page: wpPage(id: {eq: "cG9zdDo2OQ=="}) {
+      title
+      content
+    }
+    items: allWpPost(filter: {categories: {nodes: {elemMatch: {slug: {eq: "learnmore"}}}}}, sort: {fields: title, order: ASC}) {
+      nodes {
+        content
+      }
+    }
+  }
+`;
+
+const LearnMore = ({ data: { items: { nodes }, page: { title, content } } }) => (
+    <Layout>
+      <h1>{title}</h1>
+      {parse(content)}
+      {nodes.map(item => (<div>{parse(item.content)}</div>))}
+    </Layout>
+  );
+
+LearnMore.propTypes = {
+  data: PropTypes.shape().isRequired,
+};
+
+export default LearnMore;
