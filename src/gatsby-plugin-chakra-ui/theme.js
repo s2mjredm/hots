2187ac5a631086e1eb12f9155bc68d14ec5683ff
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-09-11 18:34:24 -0300

    cleans up unnecessary code

--- src/gatsby-plugin-chakra-ui/theme.js
+++ src/gatsby-plugin-chakra-ui/theme.js
@@ -2,28 +2,6 @@ import { theme } from '@chakra-ui/core';
 
 const ctheme = {
   ...theme,
-  // colors: {
-  //   transparent: 'transparent',
-  //   black: '#000',
-  //   white: '#fff',
-  //   gray: {
-  //     50: '#f7fafc',
-  //     900: '#1a202c',
-  //   },
-  // },
-  textStyles: {
-    header: {
-      fontFamily: 'News Cycle',
-      color: '#403F3F',
-      fontWeight: 'semi-bold',
-    },
-    titles: {
-      fontFamily: 'Montserrat',
-      color: '#403F3F',
-      letterSpacing: '-0.03rem',
-      fontWeight: 'bold',
-    },
-  },
   breakpoints: ['40em', '52em', '70em'],
 };
 
