0a15e99f50e83f4fb6ff2934a321bb8413a85f0e
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-09-24 16:39:49 -0300

    adds search logic #3

--- src/components/indicatorModal/index.js
+++ src/components/indicatorModal/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useStaticQuery, graphql, navigate } from 'gatsby';
+import React, { useState, useEffect } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Fuse from 'fuse.js';
 import { groupBy } from 'lodash';
 
 import './index.css';
@@ -40,11 +41,12 @@ const renderTabList = categories => {
     return (
       <CustomTab
         key={tab}
-        h="46px"
+        h="56px"
         w="100%"
         justifyContent="space-between"
         borderBottom="3px solid white"
         fontFamily="Roboto"
+        textAlign="start"
         _selected={{
           bg: 'white',
           borderLeft: '5px solid #FFD285',
@@ -62,10 +64,12 @@ const renderTabPanel = indicators => {
   return indicators.map(i => {
     return (
       <PseudoBox
+        key={i}
         py={2}
         px={4}
         bg="white"
         fontFamily="News Cycle"
+        textAlign="start"
         fontSize="18px"
         borderBottom="1px solid #E5E5E5"
         color="#707070"
@@ -94,24 +98,45 @@ const IndicatorModal = ({ onClose, isOpen }) => {
       }
     `
   );
-  const categories = groupBy(
-    nodes.map(n => ({ ...n, tags: n.tags.split(';') })),
-    'category'
+  const [inputValue, setInputValue] = useState('');
+
+  const [searchResults, setSearchResults] = useState(nodes);
+
+  const [categories, setCategories] = useState(
+    groupBy(
+      searchResults.map(n => ({ ...n, tags: n.tags.split(';') })),
+      'category'
+    )
   );
 
+  useEffect(() => {
+    setCategories(
+      groupBy(
+        searchResults.map(n => ({ ...n, tags: n.tags.split(';') })),
+        'category'
+      )
+    );
+  }, [searchResults]);
+
+  const handleChange = event => {
+    setInputValue(event.target.value);
+
+    const options = {
+      includeScore: false,
+      keys: ['category'],
+    };
+
+    const fuse = new Fuse(nodes, options);
+
+    const results = fuse.search(event.target.value).map(i => i.item);
+    const newItemsList = results.length > 0 ? results : nodes;
+    setSearchResults(newItemsList);
+  };
+
   return (
-    <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose} isCentered>
+    <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
       <ModalOverlay className="blur" bg="#2a69acc9" />
-      <Flex
-        w="100%"
-        h="100vh"
-        top="0"
-        left="0"
-        align="center"
-        justify="center"
-        position="absolute"
-        zIndex="1301"
-      >
+      <Flex w="100%" h="100vh" top="0" left="0" justify="center" position="absolute" zIndex="1301">
         <Box width="600px">
           <Flex justify="space-between" color="white" align="center">
             <Text fontSize="38px" fontFamily="jubilat">
@@ -124,6 +149,8 @@ const IndicatorModal = ({ onClose, isOpen }) => {
               <InputGroup fontFamily="Roboto Slab" size="lg" color="gray.300">
                 <Input
                   autoComplete="off"
+                  onChange={e => handleChange(e)}
+                  value={inputValue}
                   placeholder="Search"
                   fontFamily="Roboto Slab"
                   color="gray.600"
@@ -131,22 +158,23 @@ const IndicatorModal = ({ onClose, isOpen }) => {
                   maxW="285px"
                   borderRadius="100px"
                 />
-                <InputRightElement children={<Icon h="40px" name="search" />} />
+                <InputRightElement>
+                  <Icon h="40px" name="search" />
+                </InputRightElement>
               </InputGroup>
             </Flex>
-          <Tabs variant="unstyled" display="flex">
-            <TabList minWidth="256px" flexDirection="column" borderRight="1px solid #F2F2F2">
-              {renderTabList(Object.keys(categories))}
-            </TabList>
-            <TabPanels width="100%" bg="#F7F7F7">
-              {Object.values(categories).map(category => {
-                <TabPanel>
-                  <p>two!</p>
-                const indicators = category.map(c => c.title);
-                return <TabPanel>{renderTabPanel(indicators)}</TabPanel>;
-              })}
-            </TabPanels>
-          </Tabs>
+            <Tabs variant="unstyled" display="flex">
+              <TabList minWidth="276px" flexDirection="column" borderRight="1px solid #F2F2F2">
+                {renderTabList(Object.keys(categories))}
+              </TabList>
+              <TabPanels width="100%" bg="#F7F7F7">
+                {Object.values(categories).map(category => {
+                  const indicators = category.map(c => c.title);
+                  return <TabPanel key={category}>{renderTabPanel(indicators)}</TabPanel>;
+                })}
+              </TabPanels>
+            </Tabs>
+          </Box>
         </Box>
       </Flex>
     </Modal>
