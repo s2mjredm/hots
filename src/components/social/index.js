f85a60e0649f7e6dcca543278870bcdb66793647
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-10-12 13:57:42 -0300

    add embed icon

--- src/components/social/index.js
+++ src/components/social/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-import { useStaticQuery, graphql } from 'gatsby';
-
 import { FacebookShareButton, TwitterShareButton, PinterestShareButton } from 'react-share';
 
 import { Box, Button, CloseButton, Flex, Icon, Modal, ModalOverlay, Text } from '@chakra-ui/core';
 
 const Social = ({ onClose, isOpen, url }) => {
-  // const { site } = useStaticQuery(
-  //   graphql`
-  //     query {
-  //       site {
-  //         siteMetadata {
-  //           defaultTitle: title
-  //           titleTemplate
-  //           defaultDescription: description
-  //           siteUrl: url
-  //           defaultImage: image
-  //         }
-  //       }
-  //     }
-  //   `
-  // );
-
   const handleCopyLink = () => {
     navigator.clipboard.writeText(url);
   };
@@ -32,14 +14,14 @@ const Social = ({ onClose, isOpen, url }) => {
     <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
       <ModalOverlay bg="#4F5394" />
       <Flex w="100%" h="100vh" top="0" left="0" justify="center" position="absolute" zIndex="1301">
-        <Box width="600px">
+        <Box width={['360px', '600px']}>
           <Flex justify="space-between" color="white" align="center">
             <Text fontSize="38px" fontFamily="jubilat">
               Share
             </Text>
             <CloseButton size="lg" fontSize="32px" onClick={onClose} />
           </Flex>
-          <Flex bg="white" p={10} direction="column" align="center">
+          <Flex bg="white" py={10} px={[5, 10]} direction="column" align="center">
             <Flex justify="space-between" width="360px" p={5}>
               <Flex direction="column" align="center" justify="center" cursor="pointer">
                 <Flex
@@ -50,7 +32,8 @@ const Social = ({ onClose, isOpen, url }) => {
                   bg="#F7F7F7"
                   borderRadius="100%"
                 >
-                  <Icon name="twitter" color="white" fontSize="30px" />
+                  <Icon name="chevron-left" color="#707070" fontSize="30px" />
+                  <Icon name="chevron-right" color="#707070" fontSize="30px" />
                 </Flex>
                 <Text fontSize="14px" fontFamily="Proxima Nova" paddingTop={2}>
                   Embed
