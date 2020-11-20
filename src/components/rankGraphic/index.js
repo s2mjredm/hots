e4530e3920432fa0bbe05359992949337228c2e8
Davi Teodoro <davi.a.teodoro@gmail.com>
2020-11-19 21:18:12 -0300

    adds animations on desktop (#108)

--- src/components/rankGraphic/index.js
+++ src/components/rankGraphic/index.js
@@ -5,6 +5,7 @@ import Img from 'gatsby-image';
 import { Box, Heading, Text } from '@chakra-ui/core';
 import SwiperCore, { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useSprings, animated } from 'react-spring';
 
 import format from '../../utils/numberFormat';
 
@@ -53,6 +54,13 @@ const RankGraphic = ({ rankings }) => {
     }
   `);
 
+  const [springs, set] = useSprings(rankings.length, () => ({
+    height: 0,
+    opacity: 0,
+    marginTop: 110,
+    transform: 'scaleY(0)',
+  }));
+
   const [numSlides, setNumSlides] = useState(5);
 
   useEffect(() => {
@@ -74,75 +82,125 @@ const RankGraphic = ({ rankings }) => {
         pagination={{ clickable: true }}
         centeredSlides
         slidesPerView={numSlides}
-        style={{ alignItems: 'center' }}
+        style={{
+          alignItems: 'center',
+
+          height: '355px',
+        }}
       >
-        {rankings.map((r, i) => (
-          <SwiperSlide key={r.variable}>
-            {({ isActive, isPrev, isNext }) => (
-              <Box
-                bg="white"
-                shadow="lg"
-                order={order[i]}
-                minW={220}
-                py={5}
-                mt={30}
-                color="#184595"
-              >
-                <Box
-                  w={60}
-                  h={60}
-                  borderRadius={30}
-                  bg="#184595"
-                  position="relative"
-                  bottom={50}
-                  left={80}
-                  mb={-50}
-                  pt="5px"
-                  lineHeight="40px"
-                  textAlign="center"
-                  color="white"
-                  fontFamily="Jubilat"
-                  fontSize={24}
-                >
-                  {r.ranking}
-                  <Text as="div" fontSize="10px" lineHeight="1em" mt="-10px">
-                    RANK
-                  </Text>
-                </Box>
-                <Heading as="h2" fontFamily="Jubilat" fontSize={24} mx={5}>
-                  {r.condition}
-                </Heading>
-                {isActive && (
-                  <>
-                    <Img
-                      style={{ margin: '10px 0' }}
-                      fluid={
-                        images[r.condition.replace(/\W/, '').toLowerCase()].childImageSharp.fluid
-                      }
-                    />
-                    <Text
-                      fontFamily="Jubilat"
-                      fontSize={48}
-                      color="white"
-                      lineHeight="50px"
-                      position="relative"
-                      bottom={60}
-                      left="5px"
-                      mb="-50px"
-                    >
-                      {format(r.value, r.unit, r.rounding, r.decimals, r.factor)}
-                    </Text>
-                  </>
-                )}
-                {(isActive || isPrev || isNext) && (
-                  <Text mx={5} minH={72}>
-                    {r.conditionDefinition}
-                  </Text>
-                )}
-              </Box>
-            )}
-          </SwiperSlide>
-        ))}
+        {rankings.map((r, i) => {
+          return (
+            <SwiperSlide key={r.variable} style={{ height: 'initial' }}>
+              {({ isActive, isPrev, isNext }) => {
+                if (isActive) {
+                  set(index => {
+                    if (index !== i) return null;
+                    return {
+                      height: 160,
+                      opacity: 1,
+                      marginTop: 0,
+                      transform: 'scaleY(1)',
+                    };
+                  });
+                } else if (isNext) {
+                  set(index => {
+                    if (index !== i) return null;
+                    return {
+                      height: 0,
+                      opacity: 0,
+                      marginTop: 80,
+                      transform: 'scaleY(0)',
+                    };
+                  });
+                } else if (isPrev) {
+                  set(index => {
+                    if (index !== i) return null;
+                    return {
+                      height: 0,
+                      opacity: 0,
+                      marginTop: 80,
+                      transform: 'scaleY(0)',
+                    };
+                  });
+                } else {
+                  set(index => {
+                    if (index !== i) return null;
+                    return {
+                      height: 0,
+                      opacity: 0,
+                      marginTop: 110,
+                      transform: 'scaleY(0)',
+                    };
+                  });
+                }
+
+                return (
+                  <animated.div style={{ marginTop: springs[i].marginTop }}>
+                    <Box bg="white" shadow="lg" minW={220} py={5} mt={30} color="#184595">
+                      <Box
+                        w={60}
+                        h={60}
+                        borderRadius={30}
+                        bg="#184595"
+                        position="relative"
+                        bottom={50}
+                        left={80}
+                        mb={-50}
+                        pt="5px"
+                        lineHeight="40px"
+                        textAlign="center"
+                        color="white"
+                        fontFamily="Jubilat"
+                        fontSize={24}
+                      >
+                        {r.ranking}
+                        <Text as="div" fontSize="10px" lineHeight="1em" mt="-10px">
+                          RANK
+                        </Text>
+                      </Box>
+                      <Heading as="h2" fontFamily="Jubilat" fontSize={24} mx={5}>
+                        {r.condition}
+                      </Heading>
+                      <animated.div
+                        style={{
+                          transform: springs[i].transform,
+                          opacity: springs[i].opacity,
+                          height: springs[i].height,
+                        }}
+                      >
+                        <Img
+                          style={{ margin: '10px 0' }}
+                          fluid={
+                            images[r.condition.replace(/\W/, '').toLowerCase()].childImageSharp
+                              .fluid
+                          }
+                        />
+
+                        <Text
+                          fontFamily="Jubilat"
+                          fontSize={48}
+                          color="white"
+                          lineHeight="50px"
+                          position="relative"
+                          bottom={60}
+                          left="5px"
+                          mb="-50px"
+                        >
+                          {format(r.value, r.unit, r.rounding, r.decimals, r.factor)}
+                        </Text>
+                      </animated.div>
+                      {(isActive || isPrev || isNext) && (
+                        <Text mx={5} minH={72}>
+                          {r.conditionDefinition}
+                        </Text>
+                      )}
+                    </Box>
+                  </animated.div>
+                );
+              }}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </Box>
   );
